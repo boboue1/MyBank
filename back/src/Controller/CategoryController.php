db45fb0b7dca29344e711ec70cd4972e3e5b5f8e
boboue1 <boboueb@gmail.com>
2026-07-06 05:17:18 +0200

    Auth: update routes + CategoryController

--- back/src/Controller/CategoryController.php
+++ back/src/Controller/CategoryController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 class CategoryController extends AbstractController
@@ -18,5 +21,35 @@ class CategoryController extends AbstractController
             'id'    => $c->getId(),
             'title' => $c->getTitle(),
         ], $categories));
+    }
+
+    #[Route('/api/categories', name: 'api_categories_create', methods: ['POST'])]
+    public function create(Request $request, EntityManagerInterface $em): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        if (empty($data['title'])) {
+            return $this->json(['error' => 'Title is required'], 400);
+        }
+
+        $category = new Category();
+        $category->setTitle($data['title']);
+
+        $em->persist($category);
+        $em->flush();
+
+        return $this->json([
+            'id'    => $category->getId(),
+            'title' => $category->getTitle(),
+        ], 201);
+    }
+
+    #[Route('/api/categories/{id}', name: 'api_categories_delete', methods: ['DELETE'])]
+    public function delete(Category $category, EntityManagerInterface $em): JsonResponse
+    {
+        $em->remove($category);
+        $em->flush();
+
+        return $this->json(null, 204);
     }
 }
