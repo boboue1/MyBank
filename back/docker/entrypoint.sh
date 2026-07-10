f764b19cf7499cd9036f0c0fd0b9e52c650e9a68
boboue1 <boboueb@gmail.com>
2026-07-10 16:29:30 +0200

    fix(deploy) : definitif Dockerfile & add railway.toml

--- back/docker/entrypoint.sh
+++ back/docker/entrypoint.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 set -e
 
-php bin/console cache:clear --env=prod --no-debug
-php bin/console doctrine:migrations:migrate --no-interaction --env=prod
+# Generate JWT keys if missing
+if [ ! -f config/jwt/private.pem ]; then
+    mkdir -p config/jwt
+    openssl genpkey -algorithm RSA -out config/jwt/private.pem -aes256 -pass pass:${JWT_PASSPHRASE}
+    openssl pkey -in config/jwt/private.pem -out config/jwt/public.pem -pubout -passin pass:${JWT_PASSPHRASE}
+fi
+
+mkdir -p var/cache var/log
+
+php bin/console cache:clear --no-debug --no-warmup
+php bin/console doctrine:migrations:migrate --no-interaction --allow-no-migration
 
 exec /usr/bin/supervisord -c /etc/supervisord.conf
